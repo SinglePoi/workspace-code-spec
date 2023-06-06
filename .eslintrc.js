cbc0e037d07bf63c5ca8dd68153a6a583856f5bf
singlepoi <rotate13@outlook.com>
2023-06-06 10:44:49 +0800

    🐞 fix(eslint): 错误使用双引号

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended", "prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
