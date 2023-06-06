52e5c8666503b13ef8a5b7e6b46a560e26fa4a1e
singlepoi <rotate13@outlook.com>
2023-06-06 13:47:49 +0800

    ✨ feat: add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    parser: 'babel-eslint',
   },
   rules: {
     'no-var': 'error',
